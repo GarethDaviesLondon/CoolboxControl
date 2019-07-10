3cb46fce084d1bccab8b05c5b263673fa9035440
Gareth Davies <garethdavies.london@gmail.com>
2019-07-10 10:10:33 +0100

    Update CoolboxControl.ino

--- CoolboxControl/CoolboxControl.ino
+++ CoolboxControl/CoolboxControl.ino
@@ -242,11 +242,7 @@ void targetMaintain()
 
     startWarmCycleTemp=GlobalTemp;
     
-<<<<<<< HEAD
     while ( ( (GlobalTemp<=targetTemp+0.5) || (linevolts< alwaysOnLevel) ) && (changeMode==false) )
-=======
-    while ( (GlobalTemp<=targetTemp+0.5) && (changeMode==false) )
->>>>>>> 9765532947cedf27e16d39f3504b682857bf1f71
     {
           switchtoWarm(OFFCYCLEMINS);
           GlobalTemp=dht.readTemperature();
